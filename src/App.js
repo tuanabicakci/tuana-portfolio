3a19e84d68e44845b3ef9d9a8ba53812aae7265f
tuanabicakci <bicakcituana@gmail.com>
2023-12-02 15:54:19 -0500

    Scroll To top

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Art from "./Art";
 import Code from "./Code";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Test from "./Images/Test";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
       <Router>
           <Navbar/>
-          <Routes>
-              <Route exact path="/" element={<Home/>}/>
-              <Route path={"/art"} element={<Art/>}/>
-              <Route path={"/code"} element={<Code/>}/>
-          </Routes>
+          <ScrollToTop/>
+              <Routes>
+                  <Route exact path="/" element={<Home/>}/>
+                  <Route path={"/art"} element={<Art/>}/>
+                  <Route path={"/code"} element={<Code/>}/>
+              </Routes>
+
       </Router>
   );
 }
