79f274b305010ced0909dbfa9cbee5130c91a07c
tuanabicakci <bicakcituana@gmail.com>
2023-12-03 19:49:33 -0500

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,28 @@
 import {Link} from "react-router-dom";
 import  "./Navbar.css";
+import {FaBars, FaTimes} from "react-icons/fa";
+import {useRef} from "react";
 
 function NavBar(){
+    const navRef = useRef();
+    const showNavbar=()=>{
+        navRef.current.classList.toggle("responsive-nav")
+    }
     return(
         <header className={"navbar-wrapper"}>
             <Link to={"/"}><h1>Tuana Bicakci</h1></Link>
-            <nav className={"navbar"}>
-                <Link to={"/home"}>ALL WORK</Link>
+            <nav className={"navbar"} ref={navRef}>
+                <Link to={"/code"}>CODE</Link>
+                <Link to={"/art"}>ART</Link>
+                <Link to={"/other"}>OTHER</Link>
                 <Link to={"/about-me"}>ABOUT ME</Link>
+                <button className={"nav-btn nav-close-btn"} onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
             </nav>
+            <button className={"nav-btn"} onClick={showNavbar}>
+                <FaBars/>
+            </button>
         </header>
     );
 }
